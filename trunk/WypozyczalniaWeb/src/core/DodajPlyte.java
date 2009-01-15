446d7feb34b3734bfa205e5f95b4732aeb6455d6
jacekwiktor <jacekwiktor@866a906b-324a-0410-b6b2-635c66bbf650>
2009-01-15 17:22:30 +0000

    - pu dodaj plyte (na razie wersja uproszczonego dodawania pojedynczej plyty, dodawanie wielu na raz w drodze)

--- trunk/WypozyczalniaWeb/src/core/DodajPlyte.java
+++ trunk/WypozyczalniaWeb/src/core/DodajPlyte.java
@@ -1,6 +1,7 @@
 package core;
 
 import java.util.Collection;
+import java.util.Random;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,28 +23,30 @@ public class DodajPlyte extends AbstractController {
 		Sesja sesja = (Sesja) webApplicationContext.getBean("sesja");
 		Collection<FilmDAO> filmy = sesja.getFilmy().getAll();
 		
-		// TODO: wyswietlamy liste filmow (jak w Finder) i przy kazdym z nich pokazujemy ilosc,
-		// i dodajemy linka "zwieksz ilosc"
-		
-		/*
-		ZarzadzaniePlytamiLocal plytyMgr = sesja.getPlytyMgr();
-		Collection<FilmDAO> filmy = plytyMgr.zwrocListeFilmowDAO("300");
-		FilmDAO film = filmy.iterator().next(); // normalnie to po id
-		PlytaDAO plyta = new PlytaDAO();
-		plyta.setFilm(film);
-		plyta.setFilmWolne(film);
-		plyta.setId("1/I");
-		plytyMgr.dodajPlyte(plyta);
-
-		plyta = new PlytaDAO();
-		plyta.setFilm(film);
-		plyta.setFilmWolne(film);
-		plyta.setId("2/I");
-		plytyMgr.dodajPlyte(plyta);
-		 */
+		Integer idFilmu = -1;
+		String tekst = new String("Wybierz film, do ktorego chcesz dodac plyty.");
+		if ((arg0.getParameter("fid") != null) && (arg0.getParameter("ile") != null)) {
+			if (arg0.getParameter("ile").equals("jedna")) {
+				idFilmu = Integer.valueOf(arg0.getParameter("fid"));
+				ZarzadzaniePlytamiLocal plytyMgr = sesja.getPlytyMgr();	
+				FilmDAO film = plytyMgr.pobierzFilm(idFilmu);
+				PlytaDAO plyta = new PlytaDAO();
+				plyta.setFilm(film);
+				plyta.setFilmWolne(film);
+				Random r = new Random();
+				plyta.setId(Long.toString(r.nextLong())); // wygeneruj losowe ID
+				plytyMgr.dodajPlyte(plyta);
+				tekst = new String("Dodano plyte do filmu " + film.getTytul() + " !");		
+			} else if (arg0.getParameter("ile").equals("wiele")) {
+				// tutaj trzeba juz zmienic ModelAndView i pokazac formularz
+				tekst = "TODO";
+			}
+		}
 		
 		ModelAndView mv = new ModelAndView("dodajplyte");
 		mv.addObject("films", filmy);
+		mv.addObject("tekst", tekst);
+		mv.addObject("idFilmu", idFilmu);
 		return mv;
 	}
 
